b8cd107e050976539b611f4dc8bb83e7a4ffbe8c
midnight <jw79985@gmail.com>
2023-10-13 01:23:09 +0200

    Updated create and insert video method

--- database.py
+++ database.py
@@ -16,22 +16,22 @@ class Database:
         self.db = await aiosqlite.connect(self.file_name)
         return self.db
 
-    async def insert_video(self, table: str, title: str, msgid: str, category: str) -> bool:
+    async def insert_video(self, table: str, title: str, msgid: str, category: str, ids: str) -> bool:
         try:
-            await self.db.execute(f"INSERT INTO {table} (title,msgid,category) VALUES (?,?,?)",
-                                  (title, msgid, category,))
+            await self.db.execute(f"INSERT OR IGNORE INTO {table} (title,posterid,category,ids)"
+                                  f" VALUES (?,?,?,?)",
+                                  (title, msgid, category, ','.join(ids),))
             return True
         except aiosqlite.IntegrityError:
             return False
 
     async def create_table(self, table: str):
         page_table = f"""CREATE TABLE IF NOT EXISTS {table} (
-                        id             INTEGER PRIMARY KEY AUTOINCREMENT
-                                               UNIQUE
-                                               NOT NULL,
+                        id             INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
                         title       TEXT,
-                        msgid       TEXT,
-                        UNIQUE(msgid) ON CONFLICT FAIL                                              
+                        posterid    TEXT,
+                        category    TEXT,
+                        ids         TEXT
                     );"""
 
         await self.db.execute(page_table)
